3fe0a4bece5d2181cc89f42f8ddad4a148e9f550
khadiza21 <bibikhadiza474@gmail.com>
2025-01-24 15:43:55 +0300

    file set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/shared/Navbar/Navbar'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Footer from './components/shared/Footer/Footer'
 
 
 function App() {
 
   return (
     <>
-      
-  
-    
+      <Navbar />
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      <Footer />
 
     </>
   )
---
+++ src/components/shared/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+
+    const style = ({ isActive }) => ({
+        fontWeight: isActive ? "bold" : "normal",
+      });
+    return (
+        <div>
+            <h1>This is navbar</h1>
+        
+            <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <NavLink to="/home" style={style}>Home</NavLink>
+        <NavLink to="/about" style={style}>About</NavLink>
+      </nav>
+        </div>
+    );
+};
+
+
+
+export default Navbar;
