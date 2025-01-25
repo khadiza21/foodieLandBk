abb3953019600443a8e30171e35f05eb968c6c38
khadiza21 <bibikhadiza474@gmail.com>
2025-01-25 16:36:44 +0300

    here updated the newsletter component

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,44 +1,32 @@
-import newsletterSalad from '../../assets/images/newsletter-1.png'
-import newsletterPlate from '../../assets/images/Photo-plate.png'
-import './Newsletter.css'
+
+import "./Newsletter.css";
 const Newsletter = () => {
     return (
-        <div className=' container'>
-            <div className="newsletter my-24 py-16 px-4 md:px-8">
-                <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 items-center gap-6">
-
-                    <div className="hidden md:block relative">
-                        <img
-                            src={newsletterSalad}
-                            alt="newsletter plater"
-                            className="imageONe "
-                        />
-                    </div>
-
-
-                    <div className="text-center  w-full bg-slate-400">
-                        <div className='bg-yellow-200 newsletterBox'>
-                            <h3 className="text-3xl font-bold mb-4">Deliciousness to your inbox</h3>
-                            <p className="text-gray-600 mb-6">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.
+        <div className=" container">
+            <div className="newsletter my-24">
+                <div className="newsletterImage  py-20 ">
+                    <div className="flex items-center justify-center  ">
+                        <div className=" newsletterBox">
+                            <h3 className=" mx-auto font-bold mb-4">
+                                Deliciousness to your inbox
+                            </h3>
+                            <p className="text-gray-600 mb-6 mx-auto">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                                eiusmod tempor.
                             </p>
-                            <form className="flex justify-center items-center space-x-2 px-4">
+                            <form className="flex justify-center items-center mx-auto px-4">
                                 <input
                                     type="email"
                                     placeholder="Your email address"
                                     className=" outline-none border-0 w-full max-w-xs"
                                 />
-                                <button type="submit" className="btn ">Subscribe</button>
-                            </form></div>
-                    </div>
+                                <button type="submit" className="btn  bg-[#000000] hover:bg-slate-800 text-[#ffffff]">
+                                    Subscribe
+                                </button>
+                            </form>
+                        </div>
 
 
-                    <div className="hidden md:block relative">
-                        <img
-                            src={newsletterPlate}
-                            alt="Newsletter Salad picture"
-                            className="imageTwo"
-                        />
                     </div>
                 </div>
             </div>
