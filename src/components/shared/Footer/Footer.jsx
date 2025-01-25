05680a0a53d5bcc29a124ac17787478cb1788f76
khadiza21 <bibikhadiza474@gmail.com>
2025-01-25 08:09:10 +0300

    here added footer component and It's design done. Need to responsive the compoentent

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,8 +1,75 @@
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is footer</h1>
+        <div className="  mt-24">
+            <footer className="footer py-5 flex flex-wrap md:flex-nowrap items-center justify-between border-b border-slate-200 gap-4">
+              
+                <div className="flex-1">
+                    <h4 className="text-xl font-bold">
+                        Foodieland<span  style={{ color: "#FF7967" }} >.</span>
+                    </h4>
+                    <p className="text-sm text-gray-500">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing.
+                    </p>
+                </div>
+
+          
+                <nav className="flex-1 flex flex-wrap gap-4 justify-center md:justify-end">
+                    <a className="link link-hover text-gray-700">Recipes</a>
+                    <a className="link link-hover text-gray-700">Blog</a>
+                    <a className="link link-hover text-gray-700">Contact</a>
+                    <a className="link link-hover text-gray-700">About us</a>
+                </nav>
+            </footer>
+
+            <footer className="footer py-4 flex flex-wrap md:flex-nowrap items-center gap-4">
+
+                <p className="grow flex items-center justify-center text-center md:text-left text-sm text-gray-500">
+                    Copyright © {new Date().getFullYear()}{" "}
+                    <span style={{ color: "#FF7967" }} className="font-semibold">
+                        SpaceZee
+                    </span>
+                </p>
+
+
+                <nav className="flex gap-4 justify-center md:justify-end">
+                    <a>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            className="fill-current text-gray-700 hover:text-red-500 transition duration-200"
+                        >
+                            <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                        </svg>
+                    </a>
+                    <a>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            className="fill-current text-gray-700 hover:text-red-500 transition duration-200"
+                        >
+                            <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                        </svg>
+                    </a>
+                    <a>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            className="fill-current text-gray-700 hover:text-red-500 transition duration-200"
+                        >
+                            <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                        </svg>
+                    </a>
+                </nav>
+            </footer>
+
+
         </div>
     );
 };
